f3830b80786841cd416ebd729d00121780f17d9b
BackEndTea <gdpagter@ibuildings.nl>
2019-04-05 10:56:16 +0200

    Add heapsort

--- Python/sort/sort.py
+++ Python/sort/sort.py
@@ -26,8 +26,38 @@ def cubesort(arr):
     pass
 
 def heapsort(arr):
-    pass
+    def heapify(arr, n, i):
+        largest = i    # Initialize largest as root
+        l = 2 * i + 1  # left = 2*i + 1
+        r = 2 * i + 2  # right = 2*i + 2
 
+        # See if left child of root exists and is
+        # greater than root
+        if l < n and arr[i] < arr[l]:
+            largest = l
+
+        # See if right child of root exists and is
+        # greater than root
+        if r < n and arr[largest] < arr[r]:
+            largest = r
+
+        # Change root, if needed
+        if largest != i:
+            arr[i],arr[largest] = arr[largest],arr[i]  # swap
+
+            # Heapify the root.
+            heapify(arr, n, largest)
+
+    n = len(arr)
+
+    # Build a maxheap.
+    for i in range(n, -1, -1):
+        heapify(arr, n, i)
+
+    # One by one extract elements
+    for i in range(n-1, 0, -1):
+        arr[i], arr[0] = arr[0], arr[i]   # swap
+        heapify(arr, i, 0)
 
 def introsort(arr):
     pass
