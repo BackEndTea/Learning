1ca3b2e99b885a7d7ab532a7cdab4bfff3e65fc2
BackEndTea <gdpagter@ibuildings.nl>
2019-05-10 14:19:03 +0200

    Make a hello world C#

---
+++ C#/Program.cs
@@ -0,0 +1,15 @@
+// A Hello World! program in C#.
+using System;
+namespace HelloWorld
+{
+    class Hello
+    {
+        public static void Main()
+        {
+            Console.WriteLine("Hello World!");
+            // Keep the console window open in debug mode.
+            Console.WriteLine("Press any key to exit.");
+            Console.ReadKey();
+        }
+    }
+}
