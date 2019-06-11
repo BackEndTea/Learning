c601b320660ee9bfe37089bd30a9034881b41c59
Gert de Pagter <BackEndTea@gmail.com>
2019-06-11 21:24:30 +0200

    Add a few more Haskell functions

--- Haskell/Start/start.hs
+++ Haskell/Start/start.hs
@@ -13,3 +13,7 @@ fac x =  product [1..x]
 fac' x = if x == 1
             then x
             else x * fac (x - 1)
+
+boomBangs xs = [ if x < 10 then "BOOM!" else "BANG!" | x <- xs, odd x]
+
+length' xs = sum [1 | _ <- xs]
