de9d17459c74476e90efc50ff920265d352f8da4
BackEndTea <gdpagter@ibuildings.nl>
2019-06-17 08:45:24 +0200

    Continue with curry

---
+++ Haskell/Curry/curry.hs
@@ -0,0 +1,25 @@
+multThree x y z = x * y * z
+
+multTwoWithNine = multThree 9
+
+multWithEighteen = multTwoWithNine 2
+
+compareWithHundred :: (Num a, Ord a) => a -> Ordering
+compareWithHundred = compare 100
+
+divideByTen :: (Floating a) => a -> a
+divideByTen = (/10)
+
+isUpperAlphanum :: Char -> Bool
+isUpperAlphanum = (`elem` ['A'..'Z'])
+
+applyTwice :: (a -> a) -> a -> a
+applyTwice f x = f (f x)
+
+zipWith' :: (a -> b -> c) -> [a] -> [b] -> [c]
+zipWith' _ [] _ = []
+zipWith' _ _ [] = []
+zipWith' f (x:xs) (y:ys) = f x y : zipWith' f xs ys
+
+flip' :: (a -> b -> c) -> (b -> a -> c)
+flip' f y x = f x y
