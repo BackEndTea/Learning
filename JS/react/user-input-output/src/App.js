eebd1a6eb78f2c2d17f319236eab52d0d3e5be2a
BackEndTea <gdpagter@ibuildings.nl>
2019-06-18 14:50:21 +0200

    Use a reducer instead now

--- JS/react/user-input-output/src/App.js
+++ JS/react/user-input-output/src/App.js
@@ -1,15 +1,42 @@
-import React, {useState} from 'react';
+import React, {useReducer} from 'react';
 import UserCard from "./UserCard";
 import './UserCard.css';
 
-function Counter({initialCount}) {
-  const [count, setCount] = useState(initialCount);
+// function Counter({initialCount}) {
+//   const [count, setCount] = useState(initialCount);
+//   return (
+//     <>
+//       Count: {count}
+//       <button onClick={() => setCount(initialCount)}>Reset</button>
+//       <button onClick={() => setCount(prevCount => prevCount + 1)}>+</button>
+//       <button onClick={() => setCount(prevCount => prevCount - 1)}>-</button>
+//     </>
+//   );
+// }
+
+const initialState = {count: 0};
+
+function reducer(state, action) {
+  switch (action.type) {
+    case 'increment':
+      return {count: state.count + 1};
+    case 'decrement':
+      return {count: state.count - 1};
+    case 'reset':
+      return {count: initialState['count']};
+    default:
+      throw new Error();
+  }
+}
+
+function Counter() {
+  const [state, dispatch] = useReducer(reducer, initialState);
   return (
     <>
-      Count: {count}
-      <button onClick={() => setCount(initialCount)}>Reset</button>
-      <button onClick={() => setCount(prevCount => prevCount + 1)}>+</button>
-      <button onClick={() => setCount(prevCount => prevCount - 1)}>-</button>
+      Count: {state.count}
+      <button onClick={() => dispatch({type: 'reset'})}>Reset</button>
+      <button onClick={() => dispatch({type: 'increment'})}>+</button>
+      <button onClick={() => dispatch({type: 'decrement'})}>-</button>
     </>
   );
 }
@@ -19,7 +46,7 @@ function App() {
     <div>
       <UserCard/>
       <UserCard/>
-      <Counter initialCount={3}/>
+      <Counter />
     </div>
   );
 }
