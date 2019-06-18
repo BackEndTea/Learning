d34ec203aa614f5679766e8baa8befd1b36539de
BackEndTea <gdpagter@ibuildings.nl>
2019-06-18 13:46:05 +0200

    Remove () part of strings

--- PHP/wiki/index.php
+++ PHP/wiki/index.php
@@ -2,4 +2,19 @@
 
 $res = json_decode(file_get_contents('https://en.wikipedia.org/w/api.php?action=query&list=random&format=json&rnnamespace=0&rnlimit=1'), true);
 
-var_dump($res['query']['random'][0]['title']);
+$title = $res['query']['random'][0]['title'];
+var_dump($title);
+function delete_all_between($beginning, $end, $string) {
+    $beginningPos = strpos($string, $beginning);
+    $endPos = strpos($string, $end);
+    if ($beginningPos === false || $endPos === false) {
+        return $string;
+    }
+
+    $textToDelete = substr($string, $beginningPos, ($endPos + strlen($end)) - $beginningPos);
+
+    return delete_all_between($beginning, $end, str_replace($textToDelete, '', $string)); // recursion to ensure all occurrences are replaced
+}
+$filteredString = trim(delete_all_between('(', ')', $title));
+
+var_dump($filteredString);
