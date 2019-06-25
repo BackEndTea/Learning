23257fde8efa4fc8bcf14fb80c5f1a1807f5689a
BackEndTea <gdpagter@ibuildings.nl>
2019-06-25 09:26:25 +0200

    Add middleware

--- JS/react/burger-builder/src/index.js
+++ JS/react/burger-builder/src/index.js
@@ -2,15 +2,26 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider} from "react-redux";
-import { createStore } from "redux";
+import {createStore, applyMiddleware, compose} from "redux";
 
 import './index.css';
 import App from "./App";
 
-
 import reducer from "./store/reducer";
 
-const store = createStore(reducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+const logger = (store) => {
+  return (next) => {
+    return (action) => {
+      console.log('[MW] Dispatching:', action);
+      const result = next(action);
+      console.log('[MD] Next state: ', store.getState());
+      return result;
+    };
+  };
+};
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const store = createStore(reducer, composeEnhancers(applyMiddleware(logger)));
 
 ReactDOM.render(
   <Provider store={store}>
