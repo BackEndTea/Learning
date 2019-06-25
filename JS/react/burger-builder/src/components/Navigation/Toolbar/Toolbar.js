0142b133b1dd06cbfea53572f92996e4b72719da
BackEndTea <gdpagter@ibuildings.nl>
2019-06-25 12:26:11 +0200

    Make logo link to home page

--- JS/react/burger-builder/src/components/Navigation/Toolbar/Toolbar.js
+++ JS/react/burger-builder/src/components/Navigation/Toolbar/Toolbar.js
@@ -3,11 +3,12 @@ import classes from './Toolbar.css';
 import Logo from "../../Logo/Logo";
 import NavigationItems from "../NavigationItems/NavigationItems";
 import DrawerToggle from "../SideDrawer/DrawerToggle/DrawerToggle";
+import {NavLink} from "react-router-dom";
 
 const Toolbar = props => (
   <header className={classes.Toolbar}>
     <DrawerToggle clicked={props.drawerToggleClicked}/>
-    <div className={classes.Logo}><Logo/></div>
+    <div className={classes.Logo}><NavLink to="/"><Logo/></NavLink></div>
     <nav className={classes.DesktopOnly}>
       <NavigationItems/>
     </nav>
