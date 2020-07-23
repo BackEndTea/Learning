ccbf8fdbb8fb23643898d73e64fe7442ab46b791
Gert de Pagter <gertp93@gmail.com>
2020-07-23 08:53:38 +0200

    Implement printerErrors

--- PHP/Kata/printer_errors/printerErrors.php
+++ PHP/Kata/printer_errors/printerErrors.php
@@ -2,6 +2,20 @@
 
 namespace Kata\printer_errors;
 
+use function in_array;
+use function str_split;
+
 function printerError(string $s): string {
-    // your code
+    $allowedRange = range('a', 'm');
+
+    $total = strlen($s);
+    $incorrect = 0;
+
+    foreach(str_split($s) as $char) {
+        if(!in_array($char, $allowedRange, true)) {
+            $incorrect++;
+        }
+    }
+
+    return sprintf("%d/%d", $incorrect, $total);
 }
